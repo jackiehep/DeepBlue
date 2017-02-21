79a06e451e658d4d7b93967932116e9c9ba64ba5
jackiehep <jackie_hep@sina.com>
2017-02-21 18:44:07 +0800

    One Test
    This is one test for cpp programe!

---
+++ src/Init.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#incuude <string>
+#include <list>
+#include <map>
+#include <vector>
+#include "stdlib.h"
+using namespace std;
+class InitBase
+{
+ pirvate:
+  long m_nSize;
+ public:
+  InitBase(long nSize):m_nSize(nSize){}
+  void InitFunc()
+  {
+   cout<<m_nSize<<"********"<<endl;
+  }
+  virtual void PrnValue()=0;
+};
+class DriveClass:public InitBase
+{
+ private:
+   int m_nCnt;
+ public:
+   DriveClass(long size,int cnt):InitBase(size),m_nCnt(cnt){}
+   virtual void PrnValue()
+   {
+    InitFunc();
+    cout<<"m_nCnt<<"##################"<<endl;
+   }
+};
+int main(int argc,char * argv[])
+{
+ DriveClass Drive(222,333);
+ Drive.PrnValue();
+ return 1;
+  
+}
